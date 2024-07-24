86fa73b92659151f43e32731b213d14ef57dfeae
Krypto-Etox <prince@delhitechnicalcampus.ac.in>
2024-07-24 09:50:58 -0700

    hello program

---
+++ 1-Hello-World.cpp
@@ -0,0 +1,23 @@
+/*
+In C++, we take input and Output from the user using "cout << " and "cin >> ". We can use printf and scanf too, but in OOp it's not recommended to use.
+
+*/
+
+// Simple Input and Output.
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World";
+    return 0;
+}
+
+
+// Program 2
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world\n";
+    // or
+    std::cout << "Hello world" << std::endl; 
+    return 0;
+}
